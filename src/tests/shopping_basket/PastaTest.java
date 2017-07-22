ad9be95c02e17f58f10a3de2f305ddc75788cf88
Faisal <faisal_a@live.co.uk>
2017-07-22 20:25:07 +0100

    tests pasta buying and selling price passes

---
+++ src/tests/shopping_basket/PastaTest.java
@@ -0,0 +1,27 @@
+package shopping_basket;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PastaTest {
+
+    private Pasta pasta;
+
+    @Before
+    public void setUp() throws Exception {
+        pasta = new Pasta(1,3);
+    }
+
+    @Test
+    public void getBuyingPrice() {
+        assertEquals(1, pasta.getBuyingPrice());
+    }
+
+    @Test
+    public void getSellingPrice() {
+        assertEquals(3, pasta.getSellingPrice());
+    }
+
+}
