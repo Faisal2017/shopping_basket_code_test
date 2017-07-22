cca640cae8380742d7b7cb0e90f6e325952b8b72
Faisal <faisal_a@live.co.uk>
2017-07-22 21:02:55 +0100

    test for adding item to basket created

--- src/tests/shopping_basket/ShoppingBasketTest.java
+++ src/tests/shopping_basket/ShoppingBasketTest.java
@@ -8,10 +8,17 @@ import static org.junit.Assert.*;
 public class ShoppingBasketTest {
 
     private ShoppingBasket basket;
+    private Bread bread;
+    private Cereal cereal;
+    private Pasta pasta;
+
 
     @Before
     public void setUp() throws Exception {
         basket = new ShoppingBasket();
+        bread = new Bread(2,4);
+        cereal = new Cereal(2, 5);
+        pasta = new Pasta(1,3);
     }
 
     @Test
@@ -19,4 +26,9 @@ public class ShoppingBasketTest {
         assertEquals(0, basket.getItems());
     }
 
+    @Test
+    public void canAddItemsToBasket() {
+        basket.addItems(cereal);
+        assertEquals(1, basket.getItems());
+    }
 }
